99cb3235d59fe17613a986b550d62fa12003265d
Vinci-da-Gama <11582145@student.uts.edu.au>
2017-08-31 20:15:35 +1000

    init pj

---
+++ Day9Solu/OrderGenerator/OrderAddressL1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day9Solu.OrderGenerator
+{
+    class OrderAddressL1
+    {
+        public string Address1 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+    }
+}
